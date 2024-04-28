1144bcc8f06460c4d9c97d8d4cbd24e5282a0c77
fahad@thetechidea.com <fahad@thetechidea.com>
2024-04-28 07:01:52 +0300

    update

---
+++ Beep.Python.Winform6/uc_RunPythonTraining.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TheTechIdea.Beep.Winform.Controls.Basic;
+
+namespace Beep.Python.Winform
+{
+    [AddinAttribute(Caption = "Python Package List", Name = "uc_PackageList", misc = "AI", addinType = AddinType.Control)]
+    public partial class uc_RunPythonTraining : uc_Addin
+    {
+        public uc_RunPythonTraining()
+        {
+            InitializeComponent();
+        }
+    }
+}
