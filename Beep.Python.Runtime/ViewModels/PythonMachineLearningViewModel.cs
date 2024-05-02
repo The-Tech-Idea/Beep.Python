74091836e2f46d57799088e19855298bf22abc09
Fahed Al-Dhubaib <fahad@thetechidea.com>
2024-05-02 11:40:47 +0300

    update

---
+++ Beep.Python.Runtime/ViewModels/PythonMachineLearningViewModel.cs
@@ -0,0 +1,80 @@
+﻿using Beep.Python.Model;
+using Python.Runtime;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TheTechIdea.Beep.Container.Services;
+
+namespace Beep.Python.RuntimeEngine.ViewModels
+{
+    public class PythonMachineLearningViewModel : PythonBaseViewModel
+    {
+        public PythonMachineLearningViewModel(IBeepService beepservice, IPythonRunTimeManager pythonRuntimeManager) : base(beepservice, pythonRuntimeManager)
+        {
+            DataSetPath = PythonDatafolder;
+            
+            pythonImports = "import numpy as np\nimport pandas as pd\nfrom sklearn.model_selection import train_test_split\n import matplotlib.pyplot";
+           
+        }
+        public string DataSetPath { get; private set; }
+        public string TargetColumn { get; private set; }
+        private string pythonImports;
+        private string pythonDataPreparation { get; set; }
+        private string pythonModelTraining { get; set; }
+        private string pythonModelEvaluation { get; set; }  
+
+       
+
+        private void UpdateDataPreparationScript()
+        {
+            pythonDataPreparation = $@"
+data = pd.read_csv('{DataSetPath}')
+X = data.drop('{TargetColumn}', axis=1)
+y = data['{TargetColumn}']
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)";
+        }
+
+        public void SetModel(string modelModule, string modelName, string parameters)
+        {
+            if (!pythonImports.Contains(modelModule) && modelModule != "sklearn")
+            {
+                pythonImports += $"import {modelModule}\n";
+            }
+            pythonModelTraining = $@"
+from {modelModule} import {modelName}
+model = {modelName}({parameters})
+model.fit(X_train, y_train)";
+
+            // Check if predict_proba is appropriate and available
+            pythonModelTraining += $@"
+try:
+    model_probs = model.predict_proba(X_test)
+    has_proba = True
+except AttributeError:
+    has_proba = False
+    predictions = model.predict(X_test)  # Fallback to using predict if predict_proba is not available
+";
+        }
+        public void SetEvaluationMethod(string evaluationScript)
+        {
+            pythonModelEvaluation = evaluationScript;
+        }
+
+        public void ExecuteModel()
+        {
+            
+                try
+                {
+                    PythonRuntime.RunCode(pythonImports + pythonDataPreparation + pythonModelTraining + pythonModelEvaluation,Progress,Token);
+                    Console.WriteLine("Model executed successfully.");
+                }
+                catch (PythonException ex)
+                {
+                    Console.WriteLine($"Python Error: {ex.Message}");
+                }
+           
+        }
+    }
+}
